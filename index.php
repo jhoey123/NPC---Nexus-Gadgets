2411e5a3acbf5c7a305d5da7a735e96048d6034b
lol <73583518+lel123lel@users.noreply.github.com>
2025-03-14 20:30:07 +0800

    changed some thhings in index.php and added login.php logic

--- index.php
+++ index.php
@@ -10,33 +10,26 @@
 <body>
     <div class="d-flex justify-content-center align-items-center vh-100">
     	
-    	<form class="shadow w-450 p-5 rounded-4" 
-    	      action="php/login.php" 
-    	      method="post">
-
+    	<form class="shadow w-450 p-5 rounded-4" action="php/login.php" method="post">
     		<h4 class="display-4  fs-1">LOGIN</h4><br>
+
     		<?php if(isset($_GET['error'])){ ?>
     		<div class="alert alert-danger" role="alert">
 			  <?php echo $_GET['error']; ?>
-			</div>
+			</div>  
 		    <?php } ?>
 
 		  <div class="mb-3">
 		    <label class="form-label">User name</label>
-		    <input type="text" 
-		           class="form-control"
-		           name="uname"
-		           value="<?php echo (isset($_GET['uname']))?$_GET['uname']:"" ?>">
+		    <input type="text" class="form-control" name="uname">
 		  </div>
 
 		  <div class="mb-3">
 		    <label class="form-label">Password</label>
-		    <input type="password" 
-		           class="form-control"
-		           name="pass">
+		    <input type="password" class="form-control" name="pass">
 		  </div>
 		  
-		  <button type="submit" class="btn btn-primary">Login</button>
+		  <button type="submit" name="login" class="btn btn-primary">Login</button>
 		  <a href="index.php" class="link-secondary">Sign Up</a>
 		</form>
     </div>
