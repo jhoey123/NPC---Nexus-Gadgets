b851a71faa97b19fd6e9032a020dde955a6e88e8
lol <73583518+lel123lel@users.noreply.github.com>
2025-03-19 14:05:42 +0800

    added barcode_scan.php code

--- php/barcode_scan.php
+++ php/barcode_scan.php
@@ -1,5 +1,16 @@
 <?php 
+session_start();
+if(isset($_POST['submit'])) {
+    include "conn_db.php";
 
+    $barcode = $_POST['barcode'];
 
+    $stmt = $conn->prepare('SELECT * FROM products WHERE Barcode_id = ?');
+    $stmt->bind_param('s', $barcode);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $stmt->close();
 
+}
 ?>
