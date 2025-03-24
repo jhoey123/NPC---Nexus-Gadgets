b96bcf10c2df4a6c66e1f54614f9415b0da9f397
lol <73583518+lel123lel@users.noreply.github.com>
2025-03-24 11:12:08 +0800

    centered the text on the sidebar buttons and its icons

--- adminpanel.php
+++ adminpanel.php
@@ -38,7 +38,7 @@ if (!isset($_SESSION['user'])) {
 <body>
     <div class="sidebar">
         <div class="sidebar-icon logo">
-            <img src="images/logo.png" alt="Logo" width="24" height="24">
+            <img src="images/logo4.webp" alt="Logo" width="24" height="24">
         </div>
         <div class="sidebar-icon active" id="dashboard-button" onclick="switchView('dashboard')">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
