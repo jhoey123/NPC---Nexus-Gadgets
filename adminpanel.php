1d739bad45852ecdebcc60fccbb8dfef33c5b151
lol <73583518+lel123lel@users.noreply.github.com>
2025-04-10 00:33:47 +0800

    removed everything ABOUT SETTINS

--- adminpanel.php
+++ adminpanel.php
@@ -62,10 +62,6 @@ if (!isset($_SESSION['user'])) {
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="9" cy="21" r="1"/><circle cx="20" cy="21" r="1"/><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"/></svg>
             <span class="sidebar-text">Cart</span>
         </div>
-        <div class="sidebar-icon" id="settings-button" onclick="switchView('settings')">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="3"/><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"/></svg>
-            <span class="sidebar-text">Settings</span>
-        </div>
         <div class="sidebar-icon" id="logout-button" onclick="logout()">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" y1="12" x2="9" y2="12"/></svg>
             <span class="sidebar-text">Logout</span>
@@ -375,10 +371,6 @@ if (!isset($_SESSION['user'])) {
                 </div>
             </form>
         </div>
-    </div>
-
-    <div class="settings-content" id="settings-content" style="display: none;">
-    <div class="header-titles"> <h1>Settings Section</h1> </div>
     </div>
 
     <div class="employee-content" id="employee-content" style="display: none;">
@@ -778,7 +770,6 @@ if (!isset($_SESSION['user'])) {
             document.getElementById('upload-content').style.display = 'none';
             document.getElementById('inventory-content').style.display = 'none';
             document.getElementById('cart-content').style.display = 'none';
-            document.getElementById('settings-content').style.display = 'none';
             document.getElementById('logout-content').style.display = 'none';
             document.getElementById('employee-content').style.display = 'none';
 
