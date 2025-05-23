13c7f4f4777c56d750486594514e134f647ca5a9
VH <73583518+lel123lel@users.noreply.github.com>
2025-05-23 09:32:46 +0800

    Update conn_db.php

--- php/conn_db.php
+++ php/conn_db.php
@@ -6,9 +6,9 @@
 // $dbname = "nexus_db";
 
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "nexus_db";
+$username = "u866427573_nexus";
+$password = "@Qetu1357";
+$dbname = "u866427573_nexus";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
